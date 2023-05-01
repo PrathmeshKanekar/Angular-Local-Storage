3478127357708b5fd694c7b575a39d6144902ed9
Prathmesh Kanekar <prathmeshkanekar2002@gmail.com>
2023-05-01 13:27:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.show();
+  }
   title = 'Local-Storage';
+
+   name = "";
+  email = "";
+  Password = "";
+  persondata:any[]=[];
+  result:any;
+  subData(){
+    let person={
+      name:this.name,
+      email:this.email,
+      password:this.Password
+    }
+    this.persondata.push(person);
+
+    this.name = "";
+    this.email = "";
+    this.Password = "";
+
+
+    localStorage.setItem("data",JSON.stringify(this.persondata))
+
+
+    this.show();
+  }
+
+  show(){
+    this.result = JSON.parse(localStorage.getItem("data") || '[]')
+    console.log(this.result);
+
+
+  }
 }
